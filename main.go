bfeef6a919bc2efba4966cf3406e5570ca9efb47
Muhammad Athallah Zhuhry <muhammadzhuhry@gmail.com>
2021-02-02 22:11:08 +0700

    get data from db

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/muhammadzhuhry/bwastartup/user"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"log"
@@ -9,7 +10,7 @@ import (
 
 func main() {
 	dsn := "root:@tcp(127.0.0.1:3306)/bwastartup?charset=utf8mb4&parseTime=True&loc=Local"
-	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		// stop program dan print errornya
@@ -17,4 +18,23 @@ func main() {
 	}
 
 	fmt.Println("success connect to database")
+
+	var users []user.User
+	length := len(users)
+
+	fmt.Println(length)
+
+	// penggunaan find di gorm scr otomatis mencari ke table berdasarkan nama struct
+	// ex: struct User table users
+	db.Find(&users)
+
+	length = len(users)
+
+	fmt.Println(length)
+
+	for _, user := range users {
+		fmt.Println(user.Name)
+		fmt.Println(user.Email)
+		fmt.Println("=============")
+	}
 }
