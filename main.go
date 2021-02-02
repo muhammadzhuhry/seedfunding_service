95f97829f2df9af6f93a304a034e51889e80aacf
Muhammad Athallah Zhuhry <muhammadzhuhry@gmail.com>
2021-02-02 21:47:53 +0700

    test database connection

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"log"
+)
+
+func main() {
+	dsn := "root:@tcp(127.0.0.1:3306)/bwastartup?charset=utf8mb4&parseTime=True&loc=Local"
+	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		// stop program dan print errornya
+		log.Fatal(err.Error())
+	}
+
+	fmt.Println("success connect to database")
+}
