a14df92ee3f6ea470fb0692b554eb25444d3a027
Muhammad Athallah Zhuhry <muhammadzhuhry@gmail.com>
2021-02-03 19:48:17 +0700

    modify logic register user with repository, input and service

--- main.go
+++ main.go
@@ -16,9 +16,13 @@ func main() {
 	}
 
 	userRepository := user.NewRepository(db)
-	user := user.User{
-		Name: "Test simpan",
-	}
+	userService := user.NewService(userRepository)
 
-	userRepository.Save(user)
+	userInput := user.RegisterUserInput{}
+	userInput.Name = "Tes simpan dari service"
+	userInput.Email = "contoh@gmail.com"
+	userInput.Occupation = "artist"
+	userInput.Password = "password"
+
+	userService.RegisterUser(userInput)
 }
