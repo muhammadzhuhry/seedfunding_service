734e9b3371c395852e37d9a7ee9534aae83ad159
Muhammad Athallah Zhuhry <muhammadzhuhry@gmail.com>
2021-02-04 15:29:53 +0700

    use user formatter

--- handler/user.go
+++ handler/user.go
@@ -26,11 +26,13 @@ func (h *userHandler) RegisterUser(c *gin.Context) {
 	}
 
 	// passing input ke service
-	user, err := h.userService.RegisterUser(input)
+	newUser, err := h.userService.RegisterUser(input)
 	if err != nil {
 		c.JSON(http.StatusBadRequest, nil)
 	}
 
-	response := helper.APIResponse("Account has been registered", http.StatusOK, "success", user)
+	formatter := user.FormatUser(newUser, "tokentokentoken")
+
+	response := helper.APIResponse("Account has been registered", http.StatusOK, "success", formatter)
 	c.JSON(http.StatusOK, response)
 }
