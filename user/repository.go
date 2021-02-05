4af565b3698b481f24d4878c615f859d4aca4ac3
Muhammad Athallah Zhuhry <muhammadzhuhry@gmail.com>
2021-02-05 09:54:26 +0700

    create function query findByEmail

--- user/repository.go
+++ user/repository.go
@@ -3,7 +3,9 @@ package user
 import "gorm.io/gorm"
 
 type Repository interface {
+	// interface disini berlaku sebagai kontrak awal
 	Save(user User) (User, error)
+	FindByEmail(email string) (User, error)
 }
 
 type repository struct {
@@ -19,7 +21,17 @@ func NewRepository(db *gorm.DB) *repository {
 // proses simpan ke database
 func (r *repository) Save(user User) (User, error) {
 	err := r.db.Create(&user).Error
+	if err != nil {
+		return user, err
+	}
 
+	return user, nil
+}
+
+func (r *repository) FindByEmail(email string) (User, error) {
+	var user User
+
+	err := r.db.Where("email = ?", email).Find(&user).Error
 	if err != nil {
 		return user, err
 	}
