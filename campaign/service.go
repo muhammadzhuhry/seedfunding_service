5cae8e6c392fd27da6a01847da4e49b6925223a9
Muhammad Athallah Zhuhry <muhammadzhuhry@gmail.com>
2021-02-20 21:09:17 +0700

    create campaigns service

--- campaign/service.go
+++ campaign/service.go
@@ -1,1 +1,34 @@
 package campaign
+
+type Service interface {
+	// parameternya bukan struct karena yg user kirim bukan berbentuk json jd tidak perlu di binding
+	// melainkan langsung mengambil dari parameter
+	GetCampaigns(userID int) ([]Campaign, error)
+}
+
+type service struct {
+	repository Repository
+}
+
+func NewService(repository Repository) *service {
+	return &service{repository}
+}
+
+func (s *service) GetCampaigns(userID int) ([]Campaign, error) {
+	// jika user mengirimkan user id maka kita memanggil data campaign berdasarkan  userid yguser kirim
+	if userID != 0 {
+		campaigns, err := s.repository.FindByUserID(userID)
+		if err != nil {
+			return campaigns, err
+		}
+
+		return campaigns, nil
+	}
+
+	campaigns, err := s.repository.FindAll()
+	if err != nil {
+		return campaigns, err
+	}
+
+	return campaigns, nil
+}
