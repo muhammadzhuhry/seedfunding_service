a5296751e58eb081f7ee63415dfa8736c6eddd8c
Muhammad Athallah Zhuhry <muhammadzhuhry@gmail.com>
2021-03-07 21:47:06 +0700

    add handler authorization

--- transaction/service.go
+++ transaction/service.go
@@ -1,18 +1,33 @@
 package transaction
 
+import (
+	"errors"
+	"github.com/muhammadzhuhry/bwastartup/campaign"
+)
+
 type Service interface {
 	GetTransactionsByCampaignID(input GetCampaignTransactionsInput) ([]Transaction, error)
 }
 
 type service struct {
-	repository Repository
+	repository         Repository
+	campaignRepository campaign.Repository
 }
 
-func NewService(repository Repository) *service {
-	return &service{repository}
+func NewService(repository Repository, campaignRepository campaign.Repository) *service {
+	return &service{repository, campaignRepository}
 }
 
 func (s *service) GetTransactionsByCampaignID(input GetCampaignTransactionsInput) ([]Transaction, error) {
+	campaign, err := s.campaignRepository.FindByID(input.ID)
+	if err != nil {
+		return []Transaction{}, err
+	}
+
+	if campaign.UserID != input.User.ID {
+		return []Transaction{}, errors.New("Not an owner of this campaign")
+	}
+
 	transactions, err := s.repository.GetByCampaignID(input.ID)
 	if err != nil {
 		return transactions, err
