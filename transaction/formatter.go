9d6be8ae8f49180a07da4e6cf8f23703190ed114
Muhammad Athallah Zhuhry <muhammadzhuhry@gmail.com>
2021-03-07 21:45:56 +0700

    create format campaign transaction & format campaign transactions

--- transaction/formatter.go
+++ transaction/formatter.go
@@ -1,1 +1,36 @@
 package transaction
+
+import "time"
+
+type CampaignTransactionFormatter struct {
+	ID        int       `json:"id"`
+	Name      string    `json:"name"`
+	Amount    int       `json:"amount"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
+func FormatCampaignTransaction(transaction Transaction) CampaignTransactionFormatter {
+	campaignTransactionFormatter := CampaignTransactionFormatter{
+		ID:        transaction.ID,
+		Name:      transaction.User.Name,
+		Amount:    transaction.Amount,
+		CreatedAt: transaction.CreatedAt,
+	}
+
+	return campaignTransactionFormatter
+}
+
+func FormatCampaignTransactions(transactions []Transaction) []CampaignTransactionFormatter {
+	//if len(transactions) == 0 {
+	//	return []CampaignTransactionFormatter{}
+	//}
+	// same code short logic
+	campaignTransactionsFormatter := []CampaignTransactionFormatter{}
+
+	for _, transaction := range transactions {
+		formatter := FormatCampaignTransaction(transaction)
+		campaignTransactionsFormatter = append(campaignTransactionsFormatter, formatter)
+	}
+
+	return campaignTransactionsFormatter
+}
