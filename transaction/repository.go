5db69876ab5e3740f19f94b00a508741ba3f9c79
Muhammad Athallah Zhuhry <muhammadzhuhry@gmail.com>
2021-03-07 22:53:09 +0700

    add repository GetByUserID

--- transaction/repository.go
+++ transaction/repository.go
@@ -4,6 +4,7 @@ import "gorm.io/gorm"
 
 type Repository interface {
 	GetByCampaignID(CampaignID int) ([]Transaction, error)
+	GetByUserID(UserID int) ([]Transaction, error)
 }
 
 type repository struct {
@@ -19,6 +20,17 @@ func (r *repository) GetByCampaignID(CampaignID int) ([]Transaction, error) {
 	var transactions []Transaction
 
 	err := r.db.Preload("User").Where("campaign_id = ?", CampaignID).Order("id desc").Find(&transactions).Error
+	if err != nil {
+		return transactions, err
+	}
+
+	return transactions, nil
+}
+
+func (r *repository) GetByUserID(UserID int) ([]Transaction, error) {
+	var transactions []Transaction
+
+	err := r.db.Preload("Campaign.CampaignImages", "campaign_images.is_primary = 1").Where("user_id = ?", UserID).Find(transactions).Error
 	if err != nil {
 		return transactions, err
 	}
