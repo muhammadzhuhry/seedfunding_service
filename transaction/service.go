b889f663e11d66129d6625ef079a151bd5d87a5a
Muhammad Athallah Zhuhry <muhammadzhuhry@gmail.com>
2021-03-07 14:53:25 +0700

    create input GetCampaignTransactionInput & service GetTransactionByCampaignID

--- transaction/service.go
+++ transaction/service.go
@@ -1,1 +1,22 @@
 package transaction
+
+type Service interface {
+	GetTransactionsByCampaignID(input GetCampaignTransactionsInput) ([]Transaction, error)
+}
+
+type service struct {
+	repository Repository
+}
+
+func NewService(repository Repository) *service {
+	return &service{repository}
+}
+
+func (s *service) GetTransactionsByCampaignID(input GetCampaignTransactionsInput) ([]Transaction, error) {
+	transactions, err := s.repository.GetByCampaignID(input.ID)
+	if err != nil {
+		return transactions, err
+	}
+
+	return transactions, nil
+}
