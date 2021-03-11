0e4c9c9bc8c16fd26495fd37d5e2d01e8333e983
Muhammad Athallah Zhuhry <muhammadzhuhry@gmail.com>
2021-03-11 18:55:14 +0700

    add TransactionFormatter

--- transaction/formatter.go
+++ transaction/formatter.go
@@ -86,3 +86,27 @@ func FormatUserTransactions(transactions []Transaction) []UserTransactionFormatt
 
 	return userTransactionsFormatter
 }
+
+type TransactionFormatter struct {
+	ID         int    `json:"id"`
+	CampaignID int    `json:"campaign_id"`
+	UserID     int    `json:"user_id"`
+	Amount     int    `json:"amount"`
+	Status     string `json:"status"`
+	Code       string `json:"code"`
+	PaymentURL string `json:"payment_url"`
+}
+
+func FormatTransaction(transaction Transaction) TransactionFormatter {
+	transactionFormatter := TransactionFormatter{
+		ID:         transaction.ID,
+		CampaignID: transaction.CampaignID,
+		UserID:     transaction.UserID,
+		Amount:     transaction.Amount,
+		Status:     transaction.Status,
+		Code:       transaction.Code,
+		PaymentURL: transaction.PaymentURL,
+	}
+
+	return transactionFormatter
+}
