72e3c2399ac26df67b700fb1f2ddd80a45131eb0
Muhammad Athallah Zhuhry <muhammadzhuhry@gmail.com>
2021-03-14 23:09:17 +0700

    bugfix parameter

--- handler/transaction.go
+++ handler/transaction.go
@@ -84,7 +84,7 @@ func (h *transactionHandler) CreateTransaction(c *gin.Context) {
 	currentUser := c.MustGet("currentUser").(user.User)
 	input.User = currentUser
 
-	newTransaction, err := h.transactionService.CreateTransaction((input))
+	newTransaction, err := h.transactionService.CreateTransaction(input)
 	if err != nil {
 		response := helper.APIResponse("Failed to create transaction", http.StatusBadRequest, "error", nil)
 		c.JSON(http.StatusBadRequest, response)
